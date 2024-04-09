93e6655c671b954ec2fcedb440819d28e9061b36
Álan Ribeiro Sparremberger <alan.sparremberger@ntconsult.com.br>
2024-04-09 13:25:21 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename ='https://sparremberger.github.io/jb-inst/'>
+    
     <App />
-    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
